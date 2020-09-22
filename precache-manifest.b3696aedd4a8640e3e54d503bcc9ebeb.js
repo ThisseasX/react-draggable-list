e0a35e9639de2164401b6533f4ee6aede2ff32ad
Thisseas Xanthopoulos <thisseasx@yahoo.com>
2020-09-22 22:17:30 +0300

    Updates

---
+++ precache-manifest.b3696aedd4a8640e3e54d503bcc9ebeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b6a8d47a8575ccaa8ff619585afaff",
+    "url": "/react-draggable-list/index.html"
+  },
+  {
+    "revision": "b87ab5c686e97cfe19f8",
+    "url": "/react-draggable-list/static/js/2.0f3e24ed.chunk.js"
+  },
+  {
+    "revision": "bda6352cc1beffdb3e71c3f9415c8f1e",
+    "url": "/react-draggable-list/static/js/2.0f3e24ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbe4673a249fcc9e541",
+    "url": "/react-draggable-list/static/js/main.00b2721f.chunk.js"
+  },
+  {
+    "revision": "8ce025e1fd0d5a0a853e",
+    "url": "/react-draggable-list/static/js/runtime-main.c6c2ed45.js"
+  }
+]);
